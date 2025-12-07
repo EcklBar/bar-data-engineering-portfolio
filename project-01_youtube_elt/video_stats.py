ab4766a2bfe0a16741d14442d77b58682031cc59
EcklBar <qlbr913@gmail.com>
2025-12-07 15:09:56 +0200

    Update .gitignore and refactor video_stats.py to load API key from .env file

--- project-01_youtube_elt/video_stats.py
+++ project-01_youtube_elt/video_stats.py
@@ -1,7 +1,12 @@
 import requests
 # import json
 
-API_KEY = "AIzaSyAsBfDKYKk0qCz_x-6vzOaiMf7wik3m1_A"
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
 
 def get_playlist_id():
