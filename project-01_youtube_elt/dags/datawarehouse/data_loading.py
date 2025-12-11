e661a1375edb54287327bdbadf10a2b8c09ee794
EcklBar <qlbr913@gmail.com>
2025-12-11 16:07:37 +0200

    Refactor data loading function to read entire JSON file at once instead of yielding records; improve error logging for file not found and JSON decode errors.

--- project-01_youtube_elt/dags/datawarehouse/data_loading.py
+++ project-01_youtube_elt/dags/datawarehouse/data_loading.py
@@ -6,26 +6,17 @@ logger = logging.getLogger(__name__)
 
 
 def load_data():
+
     file_path = f"./data/YT_data_{date.today()}.json"
 
     try:
         logger.info(f"Processing file: YT_data_{date.today()}")
 
         with open(file_path, "r", encoding="utf-8") as raw_data:
-
-            for line in raw_data:
-                try:
-                    # Each line is loaded into memory one at a time
-                    record = json.load(line)
-                    # 'yield' returns one record and then pauses, waiting for the next call.
-                    # It doesn't store all records in memory.
-                    yield record
-                except json.JSONDecodeError:
-                    logger.warning(f"Skipping malformed JSON line: {line.strip()}")
-                    continue  # Go to the next line if this one is bad
-
+            data = json.load(raw_data)
+        return data
     except FileNotFoundError:
-        logger.error(f"File not found: {file_path}")
+        logger.error(f"File not found:{file_path}")
         raise
     except json.JSONDecodeError:
         logger.error(f"Invalid JSON in file: {file_path}")
